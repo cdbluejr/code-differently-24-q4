58ef6dd955e29239da27cf4c3bd66e506ba03bbe
cdbluejr <cdblue244@gmail.com>
2025-01-24 15:50:21 +0000

    Lesson 24: incomplete project 0124_01

--- lesson_24/cdbluejr_template/script.js
+++ lesson_24/cdbluejr_template/script.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 5555
+const port = 5500
 const cors = require("cors")
 
 app.use(express.urlencoded({extended:true}))
@@ -11,6 +11,19 @@ app.get("/", cors(), async(req, res) => {
     res.send("this is working")
 })
 
+app.post("/custinfo", (req, res) => {
+  const { "first-name": firstName, "middle-inital": middleInitial, "last-name": lastName, suffix, email, information } = req.body;
+  
+  console.log("Form submitted:", { firstName, middleInitial, lastName, suffix, email, information });
+
+  res.send(`
+    <h1>Information Submitted Successfully!</h1>
+    <p><strong>Complete Name:</strong> ${firstName} ${middleInitial} ${lastName} ${suffix}</p>
+    <p><strong>Email:</strong> ${email}</p>
+    <p><strong>Additional Information:</strong> ${information}</p>
+  `);
+});
+
 app.listen(port, () => {
-    console.log('this is working3333')
+    console.log('Did this really work?')
 })
